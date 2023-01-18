1215b0e8b21cbcddaa7cdbe46b1167a19170f842
ankit6823 <ankitpal8820@gmail.com>
2023-01-18 16:39:04 +0530

    update

--- preload.js
+++ preload.js
@@ -1,13 +1,5 @@
-const{ contextBridge, ipcRenderer,ipcMain } = require ('electron');
-contextBridge.exposeInMainWorld('Bridge',{
-   setKeyword : (keyword) => ipcRenderer.send("set-keyword",keyword),
-   KeyWordData : (data) => ipcRenderer.on('keyword-data',data)
-
-   
-})
-
-
-
-
-
-
+const { contextBridge, ipcRenderer, ipcMain } = require("electron");
+contextBridge.exposeInMainWorld("Bridge", {
+  setKeyword: (keyword) => ipcRenderer.send("set-keyword", keyword),
+  KeyWordData: (data) => ipcRenderer.on("keyword-data", data),
+});
